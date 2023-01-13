fa87d7137fbc9450d894c97e21a976cc9f5631e5
chon26909 <59905927+chon26909@users.noreply.github.com>
2023-01-13 23:25:45 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { connectMongoDB } from './lib/mongoDB';
 connectMongoDB();
 
 const geeting = (name: string) => {
-    console.log(`Hello 123456aomsss ${name} from TypeScript.`);
+    console.log(`Hello !!!!! ${name} from TypeScript.`);
 };
 
 geeting('Ball');
