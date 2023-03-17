a20556a412c48f30cc887c760cf638ed824deeef
Thapanee88 <76778207+Thapanee88@users.noreply.github.com>
2023-03-17 16:57:04 +0700

    Search update

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { updateUser, deleteUser } from '../controllers/userController';
+import { signUpValidation, validate } from '../middleware/vaildator';
 
 const router = express.Router();
 
-router.put('/:uid', updateUser);
+router.put('/:uid', signUpValidation(), validate, updateUser);
 router.delete('/', deleteUser);
 
 export default router;
