75c0e6066f672ce5e970e177e7ae1b36aeae76bd
Thanakrit sangsuwan <191144zs456zs@gmail.com>
2023-03-30 16:15:20 +0700

    Update

--- src/routes/toiletRoute.ts
+++ src/routes/toiletRoute.ts
@@ -1,11 +1,17 @@
 import express from 'express';
-import { createToilet, getAlltoiletPrivate, getMytoilet } from '../controllers/toiletController';
+import {
+    createToilet,
+    getAlltoiletPrivate,
+    getMytoilet,
+    updateToilet,
+} from '../controllers/toiletController';
 import { authentication } from '../middleware/verifyHeader';
 import { toiletValidation, validate } from '../middleware/vaildator';
 
 const router = express.Router();
 router.get('/', getAlltoiletPrivate);
 router.get('/mytoilet', getMytoilet);
+router.put('/updateToilet', updateToilet);
 // router.use(authentication);
 router.post('/', toiletValidation(), validate, createToilet);
 
